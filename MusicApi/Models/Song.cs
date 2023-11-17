4936549bfff415a6d32282bdb01418d5ad514556
Bharat Poptwani <bharat.poptwani@gmail.com>
2023-11-17 18:40:44 +0530

    basic example of how to work with mockdata in MVC, our level 1

---
+++ MusicApi/Models/Song.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MusicApi.Models
+{
+	public class Song
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Language { get; set; }
+	}
+}
+
