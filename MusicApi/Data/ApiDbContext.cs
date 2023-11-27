3ec6d1561b10ccf0e0c6fb8aeaef959aaa5f1e3e
Bharat Poptwani <bharat.poptwani@gmail.com>
2023-11-27 20:32:57 +0530

    added EF Core DBContext and DBSet

---
+++ MusicApi/Data/ApiDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using MusicApi.Models;
+
+namespace MusicApi.Data
+{
+	public class ApiDbContext : DbContext
+	{
+		public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+		{
+
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+			optionsBuilder.UseSqlServer("Server=localhost;Database=master;User Id=sa;Password=helloWORLD12;TrustServerCertificate=True;");
+		}
+
+        public DbSet<Song> Songs { get; set; }
+    }
+}
+
